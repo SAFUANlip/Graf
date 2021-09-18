a7b525ac2429cf803adc0f7b77aff6a77584af11
SAFUANlip <iusupov.se@phystech.edu>
2021-09-18 03:38:26 +0300

    Graf in class

--- Graf.cpp
+++ Graf.cpp
@@ -1,140 +1,30 @@
-﻿#include <iostream>
-#include <vector>
+﻿#include "Graf.h"
 using namespace std;
 
-enum color
-{
-    white,
-    gray,
-    black
-};
 
-bool is_connected_dfs(const vector<vector<int>>& g, vector<bool>& visited, int v, int v1) {
-    cout << "Current vertex " << v << endl;
 
-    visited[v] = true;
-    if (v == v1) {
-        return true;
+    int main()
+    {
+        //неорграф
+        int n, m;
+        cin >> n >> m; //k-кол рёбер
+        UnGraf to(n, m);
+        cout<<to.find_cycle();
+        //cout << find_cycle(g);
+        //cin >> first >> second;
+        //cout << is_connected(v, first, second);
+        /*vector<vector<int>> comp = components(v);
+        for (int i = 0; i < comp.size(); ++i)
+            for (int j = 0; j < comp[i].size(); ++j)
+                cout << "comp " << i << " vertex " << comp[i][j] << endl;*/
+                //cout<<find_directed_cycle(g);
+                //    for(auto x: v)
+                //    {
+                //        for(int s: x)
+                //            cout << s << " ";
+                //        cout << endl;
+                //     }
+        return 0;
+
     }
-    for (int u : g[v])
-        if (!visited[u] && is_connected_dfs(g, visited, u, v1))
-            return true;
-    return false;
-}
-
-void components_dfs(const vector<vector<int>>& g, vector<int>& c, int v, int ci) {
-    c[v] = ci;
-    for (int u : g[v])
-        if (c[u] == -1)
-            components_dfs(g, c, u, ci);
-}
-
-vector<vector<int>> components(const vector<vector<int>>& g) {
-    vector<int> c(g.size(), -1);
-    int ci = 0;
-    for (int v = 0; v < g.size(); ++v)
-        if (c[v] == -1)
-            components_dfs(g, c, v, ci++);
-    vector<vector<int>> comp(ci);
-    for (int v = 0; v < c.size(); ++v)
-        comp[c[v]].push_back(v);
-
-    return comp;
-}
-
-bool is_connected(const vector<vector<int>>& v, int first, int second) {
-    vector<bool> vis(v.size());
-    return is_connected_dfs(v, vis, first, second);
-}
-
-bool find_cycle_directed_dfs(const vector<vector<int>>& g, vector<color>& colors, int v)
-{
-    cout << "* in" << v << endl;
-    colors[v] = gray;
-    for (int u : g[v])
-        if (colors[u] == gray || (colors[u] == white && find_cycle_directed_dfs(g, colors, u)))
-            return true;
-    colors[v] = black;
-    cout << "** out" << v << endl;
-    return false;
-}
-
-bool find_directed_cycle(const vector<vector<int>>& g)
-{
-    vector<color> colors(g.size(), white);
-    for (int v = 0; v < g.size(); ++v)
-        if (colors[v] == white && find_cycle_directed_dfs(g, colors, v))
-            return true;
-    return false;
-}
-
-bool find_cycle_dfs(const vector<vector<int>>& g, vector<color>& colors, int p, int v) //p-родитель, завели для неоргафа, чтобы мы не взяли за цикл 0-1 1-0
-{
-    cout << '* in' << v << endl;
-    colors[v] = gray;
-    for (int u : g[v])
-        if (u != p && (colors[u] == gray || (colors[u] == white && find_cycle_dfs(g, colors, v, u))))
-            return true;
-    colors[v] = black;
-    cout << "** out" << v << endl;
-    return false;
-}
-
-bool find_cycle(const vector<vector<int>>& g)
-{
-    vector<color> colors(g.size(), white);
-    for (int v = 0; v < g.size(); ++v)
-        if (colors[v] == white && find_cycle_dfs(g, colors, -1, v))
-            return true;
-    return false;
-
-}
-
-void topologcy_sort_dfs(const vector<vector<int>>& g, vector<int>& res, vector<bool>& visited, int v)
-{
-    visited[v] = true;
-    for (int u : g[v])
-        if (!visited[u])
-            topologcy_sort_dfs(g, res, visited, u);
-    res.push_back(v);
-}
-
-
-void topologcy_sort(const vector<vector<int>>& g, vector<int>& res)
-{
-    vector<bool> visited(g.size());
-    for (int v = 0; v < g.size(); ++v)
-        if (!visited[v])
-            topologcy_sort_dfs(g, res, visited, v);
-    reverse(res.begin(), res.end()); //переворачиваем т.к. в начале нужны вершины, от которых исходят рёбра
-}
-
-int main()
-{
-    //неорграф
-    int n, first, second, k;
-    cin >> n >> k; //k-кол рёбер
-    vector<vector<int>> g(n);
-    for (int i = 0; i < k; ++i) {
-        cin >> first >> second;
-        g[first].push_back(second);
-        //g[second].push_back(first); //теперь неориентированный
-    }
-    //cout << find_cycle(g);
-    //cin >> first >> second;
-    //cout << is_connected(v, first, second);
-    /*vector<vector<int>> comp = components(v);
-    for (int i = 0; i < comp.size(); ++i)
-        for (int j = 0; j < comp[i].size(); ++j)
-            cout << "comp " << i << " vertex " << comp[i][j] << endl;*/
-    cout<<find_directed_cycle(g);
-    //    for(auto x: v)
-    //    {
-    //        for(int s: x)
-    //            cout << s << " ";
-    //        cout << endl;
-    //     }
-    return 0;    
-    
-}
 
---
+++ Graf.h
@@ -0,0 +1,167 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Graf {
+    
+public:
+    Graf(int n, int m) : ver{ n }, ed{ m } { g.resize(ver); };
+
+    bool connect(int first, int second) {
+        if (first >= g.size() || second >= g.size()) {
+            cout << "The graph has no such vertices";
+            return false;
+        }
+        vector<bool> vis(g.size());
+        return(is_connected_dfs(vis, first, second));
+    }
+
+    vector<vector<int>> components() {
+        vector<int> c(g.size(), -1);
+        int ci = 0;
+        for (int v = 0; v < g.size(); ++v)
+            if (c[v] == -1)
+                components_dfs(c, v, ci++);
+        vector<vector<int>> comp(ci);
+        for (int v = 0; v < c.size(); ++v)
+            comp[c[v]].push_back(v);
+        return comp;
+    }
+
+
+private:
+    bool is_connected_dfs(vector<bool>& visited, int v, int v1) {
+        cout << "Current vertex " << v << endl;
+
+        visited[v] = true;
+        if (v == v1) {
+            return true;
+        }
+        for (int u : g[v])
+            if (!visited[u] && is_connected_dfs(visited, u, v1))
+                return true;
+        return false;
+    }
+
+    void components_dfs(vector<int>& c, int v, int ci) {
+        c[v] = ci;
+        for (int u : g[v])
+            if (c[u] == -1)
+                components_dfs(c, u, ci);
+    }
+
+protected:
+    enum color
+    {
+        white,
+        gray,
+        black
+    };
+
+    int ver, ed;
+    vector<vector<int>> g;
+
+    ~Graf() {
+        g.clear();
+    }
+    virtual bool find_cycle() = 0;
+    
+};
+
+class DirGraf : public Graf {
+
+public:
+    DirGraf(int n, int m):Graf(n, m) {
+        int first, second;
+        for (int i = 0; i < ed; ++i) {
+            cin >> first >> second;
+            g[first].push_back(second);
+        }
+    }
+
+    void top_sort() {
+        if (this->find_cycle()) {
+            cout << "in cycle graf can't be top sort";
+            return;
+        }
+        topologcy_sort();
+        
+    }
+
+    bool find_cycle() override
+    {
+        vector<color> colors(g.size(), white);
+        for (int v = 0; v < g.size(); ++v)
+            if (colors[v] == white && find_cycle_directed_dfs(colors, v))
+                return true;
+        return false;
+    }
+
+private:
+    vector<int> top_s;
+
+    void topologcy_sort()
+    {
+        vector<bool> visited(g.size());
+        for (int v = 0; v < g.size(); ++v)
+            if (!visited[v])
+                topologcy_sort_dfs(visited, v);
+        reverse(top_s.begin(), top_s.end()); //переворачиваем т.к. в начале нужны вершины, от которых исходят рёбра
+    }
+
+    void topologcy_sort_dfs(vector<bool>& visited, int v)
+    {
+        visited[v] = true;
+        for (int u : g[v])
+            if (!visited[u])
+                topologcy_sort_dfs(visited, u);
+        top_s.push_back(v);
+    }
+
+    bool find_cycle_directed_dfs(vector<color>& colors, int v)
+    {
+        cout << "* in" << v << endl;
+        colors[v] = gray;
+        for (int u : g[v])
+            if (colors[u] == gray || (colors[u] == white && find_cycle_directed_dfs(colors, u)))
+                return true;
+        colors[v] = black;
+        cout << "** out" << v << endl;
+        return false;
+    }
+};
+
+class UnGraf : public Graf {
+
+public:
+    UnGraf(int n, int m) : Graf(n, m) {
+        int first, second;
+        for (int i = 0; i < ed; ++i) {
+            cin >> first >> second;
+            g[first].push_back(second);
+            g[second].push_back(first); //теперь неориентированный
+        }
+    }
+
+    bool find_cycle() override
+    {
+        vector<color> colors(g.size(), white);
+        for (int v = 0; v < g.size(); ++v)
+            if (colors[v] == white && find_cycle_dfs(colors, -1, v))
+                return true;
+        return false;
+    }
+
+private:
+    bool find_cycle_dfs(vector<color>& colors, int p, int v) //p-родитель, завели для неоргафа, чтобы мы не взяли за цикл 0-1 1-0
+    {
+        cout << "* in " << v << endl;
+        colors[v] = gray;
+        for (int u : g[v])
+            if (u != p && (colors[u] == gray || (colors[u] == white && find_cycle_dfs(colors, v, u))))
+                return true;
+        colors[v] = black;
+        cout << "** out " << v << endl;
+        return false;
+    }
+};
