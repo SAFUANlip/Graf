f832afe8793ab0bfdbed65d4d06038adf5519548
SAFUANlip <iusupov.se@phystech.edu>
2022-08-11 16:28:17 +0300

    add intro.cpp

---
+++ intro.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+
+
+int main(){
+	int a = 5;
+	int b = 7;
+	std::cout<<a+b;
+    std::string s = "HH";
+    std::string ss = "HAha";
+    std::string sss;
+    (s+ss) = sss;
+    std::cout<<sss<<'\n';
+    std::cout<<s<<' '<<ss<<'\n';
+
+}
+
+
